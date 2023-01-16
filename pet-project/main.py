8f8c0512ddeb52a4f7a115741a4dbcc4a444d4d0
Марк Аверченко <maverchenko@cinimex.ru>
2023-01-16 11:06:26 +0500

    added webRTC

--- pet-project/main.py
+++ pet-project/main.py
@@ -1,17 +1,17 @@
-from streamlit_webrtc import webrtc_streamer, WebRtcMode
+from streamlit_webrtc import webrtc_streamer, WebRtcMode, RTCConfiguration
 from game.process import VideoProcessor
 
 
-# RTC_CONFIGURATION = RTCConfiguration({
-#     "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
-# })
+RTC_CONFIGURATION = RTCConfiguration({
+    "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+})
 
 
 def main():
     webrtc_streamer(
         key="WYH",
         mode=WebRtcMode.SENDRECV,
-        # rtc_configuration=RTC_CONFIGURATION,
+        rtc_configuration=RTC_CONFIGURATION,
         media_stream_constraints={"video": True, "audio": False},
         video_processor_factory=VideoProcessor,
         async_processing=True,
